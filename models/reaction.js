71cd468b10e4bb9c0c4fc4ffa12b8caea24ac715
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-08-06 22:58:31 -0400

    work on the reaction

---
+++ models/reaction.js
@@ -0,0 +1,41 @@
+const { Schema} = require('mongoose');
+const {Type} = require('mongoose')
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => formatTimestamp(timestamp)
+    },
+  },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+      }
+    );
+    function formatTimestamp(timestamp) {
+        const options = {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        };
+        return timestamp.toLocaleDateString("en-US", options) + " at " + timestamp.toLocaleTimeString();
+      }
+      
+      module.exports = reactionSchema;
