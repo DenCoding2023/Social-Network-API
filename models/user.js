c9d8adb68e09af3a8e73fed3aeeaa16a3405b479
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-08-06 22:43:20 -0400

    update user

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const {Schema} = require('mongoose');
+const {model} = require('mongoose');
+
+const Thought = require('./Thought');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: [true, 'Username is required'],
+        trim: true,
+      },
+
+      email: {
+        type: String,
+        unique: true,
+        required: [true, 'Email is required'],
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+  
+  userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+  
+  const User = model('user', userSchema);
+  
+  module.exports = User;
